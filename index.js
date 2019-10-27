f152434f1eb3323eafb1d8853c3e8c973767b1a1
RV1989 <rien__verbeke@msn.com>
2019-10-27 19:41:32 +0100

    added tx and account

--- index.js
+++ index.js
@@ -23,6 +23,36 @@ const createBlockRelation = async (session, block) => {
     return await session.run(createRelationQuery)
 }
 
+const updateAccountInfo = async (session, accountId) => {
+    let account = await web3.eth.accounts(accountId)
+    let updateQuery = `MERGE (b:Account{id:'${accountId}' , hasCode : ${account.hasCode}})`
+    return await session.run(updateQuery)
+
+}
+const createAccountOriginRelation = async (session, tx) => {
+    let createRelationQuery = `MERGE (a:Tx{id:'${tx.id}'})
+    MERGE (b:Account{id:'${tx.origin}'})
+    CREATE (b)-[r:AccountTx]->(a)
+    RETURN type(r)`
+    return await session.run(createRelationQuery)
+}
+
+const createAccountDelegatorRelation = async (session, tx) => {
+    let createRelationQuery = `MERGE (a:Tx{id:'${tx.id}'})
+    MERGE (b:Account{id:'${tx.delegator}'})
+    CREATE (b)-[r:txDelegator]->(a)
+    RETURN type(r)`
+    return await session.run(createRelationQuery)
+}
+
+const createClauseAccountRelation = async (session, tx,clauseNumber) => {
+    let createRelationQuery = `MERGE (a:Clause{id:'${tx.id}Cl${clauseNumber}'})
+    MERGE (b:Account{id:'${tx.clauses[clauseNumber].to}'})
+    CREATE (a)-[r:clauseAccount]->(b)
+    RETURN type(r)`
+    //updateAccountInfo(session,tx.clauses[clauseNumber].to)
+    return await session.run(createRelationQuery)
+}
 const createTx = async (session, tx) => {
     let query = `CREATE (n:Tx {id : '${tx.id}'})`
     return await session.run(query)
@@ -30,19 +60,50 @@ const createTx = async (session, tx) => {
 
 const createTxRelation = async (session, tx) => {
     let createRelationQuery = `MATCH (a:Tx),(b:Block)
-    WHERE a.id = '${tx.id}' AND b.number = ${tx.meta.blockNumber}
+    WHERE a.id = '${tx.id}' AND b.number = ${tx.blockNumber}
     CREATE (a)-[r:txBlock]->(b)
     RETURN type(r)`
     return await session.run(createRelationQuery)
 }
 
+const createClause = async (session, tx,clauseNumber) => {
+    let query = `CREATE (n:Clause {id : '${tx.id}Cl${clauseNumber}'})`
+    return await session.run(query)
+}
+
+const createClauseTxRelation = async (session, tx,clauseNumber) => {
+    let createRelationQuery = `MATCH (a:Clause),(b:Tx)
+    WHERE a.id = '${tx.id}Cl${clauseNumber}' AND b.id = '${tx.id}'
+    CREATE (b)-[r:txClause]->(a)
+    RETURN type(r)`
+    return await session.run(createRelationQuery)
+}
+
+
+
 const txHandler = async (session,block) => {
     return await Promise.all(block.transactions.map( async (txId)=> {
         let tx = await web3.eth.getTransaction(txId)
-        return await Promise.all([tx,
-            createTx(session ,tx) , 
-            createTxRelation(session, tx)])
+        await createTx(session ,tx) 
+        await createTxRelation(session, tx)
+        await createAccountOriginRelation(session,tx)
+        if (tx.delegator){
+            await createAccountDelegatorRelation(session,tx)
+        }
+        
+        for (let [i,clauses] of tx.clauses.entries()){
+            await createClause(session,tx,i)
+            await createClauseTxRelation(session,tx,i)
+            await createClauseAccountRelation(session,tx,i)
+
+        }
+
+        return Promise.resolve()
+        
+
+            
     }))
+    
 }
 
 const blockHandler = async (session, blockNumber) =>{
@@ -58,9 +119,10 @@ const blockHandler = async (session, blockNumber) =>{
 const main = async () => {
     const session = driver.session()
     await DeleteAll(session)
-    let firstBlock = 0
-    let lastBlock = 1000
-    let blockNumber = _.range(firstBlock , lastBlock +1 )
+    let lastBlock = await web3.eth.getBlock('latest')
+    let firstBlock = await web3.eth.getBlock(lastBlock.number - 10000)
+    
+    let blockNumber = _.range(firstBlock.number , lastBlock.number )
 
     for (let blockNr of blockNumber){
         let result =  await blockHandler(session , blockNr)
